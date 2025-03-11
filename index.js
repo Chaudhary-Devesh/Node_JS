702800f306030cbb20a307552352f7545e4a262a
Chaudhary-Devesh <chaudharydevesh704@gmail.com>
2025-03-11 23:45:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// const math = require("./math.js");
+
+// console.log(math);
+
+// console.log(math.add(2,3));
+// console.log(math.sub(2,3));
+
+
+// console.log(math);
+const {add,sub} = require("./math.js");
+
+console.log(add(2,3));
+console.log(sub(2,3));
