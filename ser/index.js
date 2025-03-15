5e4ae944c029bf2cc4c1ba9583e29ba59d640d75
Chaudhary-Devesh <chaudharydevesh704@gmail.com>
2025-03-16 00:02:28 +0530

    express

--- ser/index.js
+++ ser/index.js
@@ -1,37 +1,70 @@
-const http = require("http");
-const fs = require("fs");
+// const http = require("http");
+// const fs = require("fs");
+// const url = require("url");
 
-myServer=http.createServer((req,res)=>{
-    const log = `${Date.now()}:${req.url} Request recieve\n`;
-    fs.appendFile('./log.txt',log,()=>{
-        switch(req.url){
-            case "/":
-                res.end("Home Page");
-                break;
-            case "/about":
-                res.end("about Page");
-                break;
-            default:
-                res.end("404");
-                break;
+// myServer=http.createServer((req,res)=>{
     
-        }
-    });
+//     if(req.url=="/favicon.ico") return res.end();
+//     const log = `${Date.now()}:${req.url}:${req.method} Request recieve\n`;
+//     const myurl = url.parse(req.url,true);
+//     console.log(myurl);
+//     fs.appendFile('./log.txt',log,()=>{
+       
+//         switch(myurl.pathname){
+//             case "/":
+//                 res.end("Home page");
+//                 break;
+//             case "/about":
+//                 const user = myurl.query.user_name;
+//                 res.end(`hello ${user}`);
+//                 break;
+//             case "/search":
+//                     const search = myurl.query.search_query;
+//                     res.end(`hello ${search}`);
+//                     break;
+//             default:
+//                 res.end("404");
+//                 break;
+    
+//         }
+//     }); 
 
-    switch(req.url){
-        case "/":
-            fs.appendFile('./log.txt',log,()=>{res.end("Home Page")});
-            break;
-        case "/about":
-            fs.appendFile('./log.txt',log,()=>{res.end("about Page")});
-            break;
-        default:
-            fs.appendFile('./log.txt',log,()=>{res.end("404")});
+// //     switch(req.url){
+// //         case "/":
+// //             fs.appendFile('./log.txt',log,()=>{res.end("Home Page")});
+// //             break;
+// //         case "/about":
+// //             fs.appendFile('./log.txt',log,()=>{res.end("about Page")});
+// //             break;
+// //         default:
+// //             fs.appendFile('./log.txt',log,()=>{res.end("404")});
 
-    }
+// //     }
 
     
     
+// })
+
+// myServer.listen(8000,()=>{console.log("server started")});
+
+const express = require('express');
+const app = express();
+
+
+app.get("/",(req,res)=>{
+    res.end("Hello Home page")
+
+
+});
+
+app.get("/search",(req,res)=>{
+    res.end(`hello ${req.query.name}`)
+
 })
 
-myServer.listen(8000,()=>{console.log("server started")});
+app.get("/about",(req,res)=>{
+    res.end("Hello about page")
+
+});
+
+app.listen(8000,()=>{console.log("server started")})
