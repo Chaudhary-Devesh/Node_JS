128d50ca0bfa2e183c519a4f1ac0d1784d7312ba
Chaudhary-Devesh <chaudharydevesh704@gmail.com>
2025-03-17 23:43:57 +0530

    post

--- restfull/index.js
+++ restfull/index.js
@@ -1,8 +1,12 @@
 const express= require('express');
 const users = require("./MOCK_DATA.json");
+const fs = require("fs");
+const { json } = require('stream/consumers');
 const app = express();
 
 const port=8000;
+
+app.use(express.urlencoded({extended:false}));
 
 app.get("/users",(req,res)=>{
     const html=`
@@ -14,16 +18,16 @@ app.get("/users",(req,res)=>{
 });
 
 
-app.get('/api/users',(req,res)=>{
-    return res.send(users);
-
-});
-// app.get('/api/users/:id',(req,res)=>{
-//     const id=Number(req.params.id);
-//     const user = users.find((user)=> user.id===id);
-//     return res.json(user);
+// app.get('/api/users',(req,res)=>{
+//     return res.send(users);
 
 // });
+app.get('/api/users/:id',(req,res)=>{
+    const id=Number(req.params.id);
+    const user = users.find((user)=> user.id===id);
+    return res.json(user);
+
+});
 // app.post('/api/users/id',(req,res)=>{
 //     return res.json({"status":"pending"});
 // })
@@ -34,14 +38,16 @@ app.get('/api/users',(req,res)=>{
 //     return res.json({"status":"pending"});
 // })
 
-app.route('/api/users/:id')
-.get((req,res)=>{
-    const id=Number(req.params.id);
-    const user = users.find((user)=> user.id===id);
-    return res.json(user);
+app.get('/api/users',(req,res)=>{
+    return res.send(users);
 
-}).post((req,res)=>{
-    return res.json({"status":"pending"});
+}).route('/api/users')
+.post((req,res)=>{
+    const body = req.body;
+    users.push({...body,id:users.length+1});
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
+        return res.json({status:"success",id:users.length});
+    });
 }).patch((req,res)=>{
     return res.json({"status":"pending"});
 }).delete((req,res)=>{
--- restfull/tempCodeRunnerFile.js
+++ restfull/tempCodeRunnerFile.js
@@ -1,2 +1,1 @@
-
-    return res.users;
+users
